d8b7199f5b33a1e7c71849f2ac5d4965d2abe5ec
yulg <yulg@dazhuanjia.com>
2021-03-04 15:18:34 +0800

    add Merge

---
+++ src/main/java/algorithm/sort/Merge.java
@@ -0,0 +1,57 @@
+package algorithm.sort;
+
+/**
+ * @author yulg
+ * @E-mail yulg@dazhuanjia.com
+ * @date 2021/3/4 1:48 下午
+ */
+public class Merge {
+
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        //将数组a[lo..hi]排序
+        if (lo >= hi) {
+            return;
+        }
+        int mid = (lo + hi) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        //将a[lo...mid]和a[mid+1...hi]归并
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {//将a[lo...hi]复制到aux[lo...hi]中
+            aux[k] = a[k];
+        }
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    /**
+     * 对元素进行比较
+     *
+     * @param v
+     * @param w
+     * @return
+     */
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+}
