cc2dc71f2e6c1f3ca63d5a36c1526b7d56ae7283
yulg <yulg@dazhuanjia.com>
2021-03-04 11:33:08 +0800

    add Shell

--- src/main/java/algorithm/sort/Shell.java
+++ src/main/java/algorithm/sort/Shell.java
@@ -6,17 +6,26 @@ import algorithm.utils.StdOut;
 /**
  * @author yulg
  * @E-mail yulg@dazhuanjia.com
- * @date 2021/3/3 5:57 下午
+ * @date 2021/3/4 10:53 上午
  */
-public class Insertion {
+public class Shell {
 
     public static void sort(Comparable[] a) {
+        //将a[]升序排列
         int N = a.length;
-        for (int i = 1; i < N; i++) {
-            // i之前是有序的
-            for (int j = i; j >= 1 && less(a[j], a[j - 1]); j--) {
-                exch(a, j, j - 1);
+        int h = 1;
+        while (h < N / 3) {
+            h = 3 * h + 1; // 1, 4, 13, 40, 121, 364, 1093...
+        }
+        while (h >= 1) {
+            //将数组变为h有序
+            for (int i = h; i < N; i++) {
+                //将a[i]插入到a[i-h],a[i-2h],a[i-3h]...之中
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j -h);
+                }
             }
+            h /= 3;
         }
     }
 
