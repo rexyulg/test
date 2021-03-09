3793a087a96ebedd058184590790f5d9d7b02162
yulg <yulg@dazhuanjia.com>
2021-03-09 15:27:03 +0800

    add BinarySearchST

---
+++ src/main/java/algorithm/search/BinarySearchST.java
@@ -0,0 +1,140 @@
+package algorithm.search;
+
+import algorithm.utils.Queue;
+
+import java.util.NoSuchElementException;
+
+/**
+ * @author yulg
+ * @E-mail yulg@dazhuanjia.com
+ * @date 2021/3/6 10:48 上午
+ */
+public class BinarySearchST<Key extends Comparable<Key>, Value> {
+
+    private Key[] keys;
+    private Value[] vals;
+    private int N;
+
+    public BinarySearchST(int capacity) {
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public Value get(Key key) {
+        if (isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            return vals[i];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 查找键，找到则更新值，否则创建新的元素
+     *
+     * @param key
+     * @param val
+     */
+    public void put(Key key, Value val) {
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            vals[i] = val;
+            return;
+        }
+        for (int j = N; j > i; j--) {
+            keys[j] = keys[j - 1];
+            vals[j] = vals[j - 1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        N++;
+    }
+
+    public int rank(Key key) {
+        int lo = 0, hi = N - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) {
+                hi = mid - 1;
+            } else if (cmp > 0) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return lo;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void delete(Key key) {
+
+    }
+
+    public Key min() {
+        return keys[0];
+    }
+
+    public Key max() {
+        return keys[N - 1];
+    }
+
+    public Key select(int k) {
+        return keys[k];
+    }
+
+    public Key ceiling(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to ceiling() is null");
+        }
+        int i = rank(key);
+        if (i == N) {
+            throw new NoSuchElementException("argument to ceiling() is too large");
+        } else {
+            return keys[i];
+        }
+    }
+
+    public Key floor(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to floor() is null");
+        }
+        int i = rank(key);
+        if (i < N && key.compareTo(keys[i]) == 0) {
+            return keys[i];
+        }
+        if (i == 0) {
+            throw new NoSuchElementException("argument to floor() is too small");
+        } else {
+            return keys[i-1];
+        }
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> queue = new Queue<>();
+        for (int i = rank(lo); i < rank(hi); i++) {
+            queue.enqueue(keys[i]);
+        }
+        if (contains(hi)) {
+            queue.enqueue(keys[rank(hi)]);
+        }
+        return queue;
+    }
+
+    private boolean contains(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+}
