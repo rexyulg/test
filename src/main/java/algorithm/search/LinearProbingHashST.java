16e0d2d9481486c4f7bbb2bd57c3913e05bdfad8
yulg <yulg@dazhuanjia.com>
2021-03-11 21:10:43 +0800

    add Linear..

---
+++ src/main/java/algorithm/search/LinearProbingHashST.java
@@ -0,0 +1,68 @@
+package algorithm.search;
+
+/**
+ * @author yulg
+ * @E-mail yulg@dazhuanjia.com
+ * @date 2021/3/11 3:29 下午
+ */
+public class LinearProbingHashST<Key, Value> {
+    //N<M
+    //符号表中键值对的总数
+    private int N;
+    //线性探测表的大小
+    private int M;
+    private Key[] keys;
+    private Value[] vals;
+
+    public LinearProbingHashST() {
+        keys = (Key[]) new Object[M];
+        vals = (Value[]) new Object[M];
+    }
+
+    public LinearProbingHashST(int cap) {
+        keys = (Key[])   new Object[cap];
+        vals = (Value[]) new Object[cap];
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    private void resize(int cap) {
+        LinearProbingHashST<Key, Value> t = new LinearProbingHashST<>(cap);
+        for (int i = 0; i < M; i++) {
+            if (keys[i] != null) {
+                t.put(keys[i], vals[i]);
+            }
+        }
+        keys = t.keys;
+        vals = t.vals;
+        M = t.M;
+    }
+
+    public void put(Key key, Value val) {
+        if (N >= M / 2) {
+            //将M加倍
+            resize(2 * M);
+        }
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                vals[i] = val;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = val;
+        N++;
+    }
+
+    public Value get(Key key) {
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                return vals[i];
+            }
+        }
+        return null;
+    }
+}
