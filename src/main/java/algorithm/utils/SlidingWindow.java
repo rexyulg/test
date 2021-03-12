215eb59386788edd3b6c1cfecb8be785247845dc
yulg <yulg@dazhuanjia.com>
2021-03-12 22:13:14 +0800

    modify SlidingWindow

--- src/main/java/algorithm/utils/SlidingWindow.java
+++ src/main/java/algorithm/utils/SlidingWindow.java
@@ -17,12 +17,16 @@ public class SlidingWindow {
      * @param t
      */
     void slidingWindow(String s, String t) {
+        //needs 和 window 相当于计数器，分别记录 T 中字符出现次数和「窗口」中的相应字符的出现次数
         Map<Character, Integer> need = new HashMap<>(), window = new HashMap<>();
         for (char c : t.toCharArray()) {
             need.put(c, need.getOrDefault(c, 0) + 1);
         }
         int lo = 0, hi = 0;
+        //valid 变量表示窗口中满足 need 条件的字符个数，如果 valid 和 need.size 的大小相同，则说明窗口已满足条件，已经完全覆盖了串 T
         int valid = 0;
+        // 记录最小覆盖子串的起始索引及长度
+        //0 int start = 0, len = Integer.MAX_VALUE;
         while (hi < s.length()) {
             // c是将移入窗口的字符
             char c = s.charAt(hi);
@@ -32,7 +36,7 @@ public class SlidingWindow {
             //1....
 
             /* debug输出的位置 */
-            System.out.printf("window: [%d, %d)\n%n", lo, hi);
+            //System.out.printf("window: [%d, %d)\n%n", lo, hi);
 
             //判断左侧窗口是否要搜索
             while(/*2.window needs shrink*/true) {
